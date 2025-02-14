b1388812b39507eb9d280be14a41eeb23b27132d
vishnuka <vishnukayahan84@gmail.com>
2025-02-14 16:31:02 +0530

    Design the folder strucure

---
+++ src/main/java/au/com/visiondigital/weatherDataBackend/advisor/GlobalExceptionHandler.java
@@ -0,0 +1,10 @@
+package au.com.visiondigital.weatherDataBackend.advisor;
+
+/**
+ * Author: Vishnuka Yahan De Silva
+ * User:macbookair
+ * Date:2025-02-14
+ * Time:16:24
+ */
+public class GlobalExceptionHandler {
+}
