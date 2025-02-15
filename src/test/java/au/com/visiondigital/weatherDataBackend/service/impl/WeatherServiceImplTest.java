633a7135625034a089f0bfc227db8a1eb0b99ea8
vishnuka <vishnukayahan84@gmail.com>
2025-02-15 12:58:05 +0530

    weather  serviceimpl  test part complete

--- src/test/java/au/com/visiondigital/weatherDataBackend/service/impl/WeatherServiceImplTest.java
+++ src/test/java/au/com/visiondigital/weatherDataBackend/service/impl/WeatherServiceImplTest.java
@@ -12,18 +12,18 @@ import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.web.reactive.function.client.WebClient;
 
 import static org.junit.jupiter.api.Assertions.*;
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.*;
 
 /**
  * Author: Vishnuka Yahan De Silva
  * User:vishnuka
  * Date:2025-02-15
- * Time:11:45
+ * Time:11:30
  */
 
 @ExtendWith(MockitoExtension.class)
 class WeatherServiceImplTest {
+
     @Mock
     private WebClient webClient;
 
@@ -31,30 +31,25 @@ class WeatherServiceImplTest {
     private WeatherServiceImpl weatherService;
 
     @BeforeEach
-    void setUp(){
+    void setUp() {
         weatherService = new WeatherServiceImpl(webClient);
-
     }
 
     @Test
-    void testGetWeatherSummary_Success(){
+    void testGetWeatherSummary_Success() {
         String city = "Colombo";
-        String jsonResponse = "{ \"city\": {\"name\": \"Colombo\"}," +
-                " \"list\": [{\"main\": {\"temp\": 300}, " +
-                "\"dt_txt\": \"2025-02-15 12:00:00\"}]}";
+        String jsonResponse = "{ \"city\": {\"name\": \"Colombo\"}, \"list\": [{\"main\": {\"temp\": 300}, \"dt_txt\": \"2025-02-15 12:00:00\"}]}";
 
-        //mock api resp
+        // Mock API response
         when(webClient.get())
                 .thenReturn(mock(WebClient.RequestHeadersUriSpec.class));
+
         WeatherData result = weatherService.getWeatherSummary(city);
 
         assertNotNull(result);
         assertEquals("Colombo", result.getCity());
-
     }
 
-
-    //city not found
     @Test
     void testGetWeatherSummary_CityNotFound() {
         String city = "InvalidCity";
@@ -62,6 +57,4 @@ class WeatherServiceImplTest {
 
         assertThrows(CityNotFoundException.class, () -> weatherService.getWeatherSummary(city));
     }
-
-
 }
